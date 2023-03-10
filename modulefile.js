d3ec048c2e623a754ada1fab0891243d08fbfa46
leejaekwon <nowkwon97@gmail.com>
2023-03-10 12:02:45 +0900

    matchMedia()를 활용해 반응형 구조 함수 작성

--- modulefile.js
+++ modulefile.js
@@ -49,3 +49,21 @@ const _EXAMDATA = {
     breakPoint : [ "768px", "1280px",  "1920px"]
   }
 }
+let mobile = "test1";
+let tablet = "test2";
+let desktop = "test3";
+
+// 사이즈의 조절마다 반영을 하기 위해
+// 수동으로 새로고침을 해야하는 작업이 필요하다
+// 자동으로 새로고침 하는 법 탐구 필요
+
+if (matchMedia("screen and (max-width : 767px)").matches) {
+  // 화면의 width가 767px 이하일 때
+  console.log(mobile);
+}else if (matchMedia("screen and (max-width : 1279px)").matches) {
+  // 화면의 width가 1279px 이하일 때
+  console.log(tablet);
+}else if (matchMedia("screen and (min-width : 1280px)").matches) {
+  // 화면의 width가 1280px 이상일 때
+  console.log(desktop);
+}
